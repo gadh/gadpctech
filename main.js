b6e5910447da7dbdcd3072207cec48421f594a58
liadk07 <91607695+liad07@users.noreply.github.com>
2023-03-15 22:12:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ var whatsapp="https://api.whatsapp.com/send/?phone=972548092366&text=texthere&ty
 const blankTargetElements = document.querySelectorAll('[target="_blank"]');
 var y=document.getElementsByTagName("h3");
 for (let i = 0; i < blankTargetElements.length; i++) {
-    blankTargetElements[i].href=whatsapp.replaceAll("texthere","שלום הגעתי אליך דרך האתר אני מעוניין בפריט -"+y[i].textContent)
+    blankTargetElements[i].href=whatsapp.replaceAll("texthere","שלום הגעתי אליך דרך האתר אני מעוניין בפריט -"+y[i+1].textContent)
 }
 //email
 const nonBlankTargetElements = document.querySelectorAll('a:not([target="_blank"])');
 var email="mailto:gadh98@gmail.com?subject=הזמנת מוצר&body=texthere";
 for (let i = 0; i < nonBlankTargetElements.length; i++) {
-    var temp =email.replace("הזמנת מוצר","-הזמנת מוצר"+y[i].textContent)
-    nonBlankTargetElements[i].href=temp.replaceAll("texthere","שלום הגעתי אליך דרך האתר אני מעוניין בפריט -"+y[i].textContent)
+    var temp =email.replace("הזמנת מוצר","-הזמנת מוצר"+y[i+1].textContent)
+    nonBlankTargetElements[i].href=temp.replaceAll("texthere","שלום הגעתי אליך דרך האתר אני מעוניין בפריט -"+y[i+1].textContent)
 
 }
